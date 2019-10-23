e8b06bb4417748dad9c93a7d4f1c0e3f0bb35419
Aaron Chevalier <atgc@bu.edu>
2019-10-23 16:37:53 -0400

    still fixing bug

--- data-raw/create_cosmicResult.R
+++ data-raw/create_cosmicResult.R
@@ -1,4 +1,4 @@
-cosmic=read.table("cosmic_signatures.txt", header=TRUE, stringsAsFactors = FALSE, sep="\t")
+cosmic=read.table("./data-raw/cosmic_signatures.txt", header=TRUE, stringsAsFactors = FALSE, sep="\t")
 cosmic_mat=cosmic[,4:ncol(cosmic)]
 rownames(cosmic_mat)=paste(cosmic$Substitution.Type, cosmic$Trinucleotide, sep="_")
 cosmic_result <- new("Result", signatures = as.matrix(cosmic_mat), samples = matrix(), type = "Cosmic")
