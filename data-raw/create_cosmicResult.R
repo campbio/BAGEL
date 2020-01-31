caa62e24bd22ab39a60b8f5f7babe0fcd9232aa2
Aaron Chevalier <atgc@bu.edu>
2020-01-31 15:40:06 -0500

    recreate cosmic_result as cosmic_v2_sigs

--- data-raw/create_cosmicResult.R
+++ data-raw/create_cosmicResult.R
@@ -2,5 +2,5 @@ cosmic=read.table("./data-raw/cosmic_signatures.txt", header=TRUE, stringsAsFact
 cosmic_mat=cosmic[,4:ncol(cosmic)]
 rownames(cosmic_mat)=paste(cosmic$Substitution.Type, cosmic$Trinucleotide, sep="_")
 colnames(cosmic_mat)=paste("Signature", seq_len(30), sep="")
-cosmic_result <- new("Result", signatures = as.matrix(cosmic_mat), samples = matrix(), type = "Cosmic")
-usethis::use_data(cosmic_result, internal = TRUE)
+cosmic_v2_sigs <- new("Result", signatures = as.matrix(cosmic_mat), samples = matrix(), type = "Cosmic")
+usethis::use_data(cosmic_v2_sigs, internal = TRUE)
